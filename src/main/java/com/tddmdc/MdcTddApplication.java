fa3721fda939f284837ad945003777b0bdf68b2c
raunerlucas <raunerlucas@gmail.com>
2024-03-04 14:04:33 -0300

    Initial commit Create Project

---
+++ src/main/java/com/tddmdc/MdcTddApplication.java
@@ -0,0 +1,13 @@
+package com.tddmdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdcTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdcTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tddmdc/MdcTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tddmdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdcTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
