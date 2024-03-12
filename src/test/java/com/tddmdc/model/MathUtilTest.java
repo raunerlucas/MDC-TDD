b0db745e16180931953820c19fb3f925658d93ef
raunerlucas <raunerlucas@gmail.com>
2024-03-12 10:27:26 -0300

    MDC Tests P1..P6 refctory

--- src/test/java/com/tddmdc/model/MathUtilTest.java
+++ src/test/java/com/tddmdc/model/MathUtilTest.java
@@ -1,67 +1,52 @@
 package com.tddmdc.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
-import  static com.tddmdc.model.MathUtil.*;
+import static com.tddmdc.model.MathUtil.mdc;
+
 class MathUtilTest {
 
-    @Test
-    void mdcParP1() {
-        Integer a = 8, b = 4, esp = 4, obt = MathUtil.mdc(a, b);
-        assertEquals(esp, obt);
+    @ParameterizedTest
+    @CsvSource({
+            "9,3,3",
+            "4,2,2",
+            "8,4,4",
+            "7,3,1"
+    })
+    void mdcP1(final Integer a, final Integer b, final Integer esp) {
+        assertEquals(esp, mdc(a, b));
     }
 
-    @Test
-    void mdcImparP1() {
-        final Integer a = 9, b = 3, esp = 3, obt = MathUtil.mdc(a, b);
-        assertEquals(esp, obt);
-    }
-
-    @Test
-    void mdcMaxValP1() {
-        final Integer a = Integer.MAX_VALUE, b = Integer.MIN_VALUE + 1,
-                esp = Math.abs(Integer.MIN_VALUE + 1),
-                obt = MathUtil.mdc(a, b);
-        assertEquals(esp, obt);
-    }
-
-    @Test
-    void mdcParP2() {
+   @Test
+    void mdcP2() {
         final Integer a = 16, b = 8, div = 4, obt = MathUtil.mdc(a, b);
 //        assertEquals(0, obt % div);
         assertTrue(obt % div == 0);
     }
 
-    @Test
-    void mdcNegativo_AP2() {
-        final Integer a = -30, b = 0, esp = 30, obt = MathUtil.mdc(a, b);
-        assertEquals(esp, obt);
+
+    @ParameterizedTest
+    @CsvSource({
+            "-30,0,30",
+            "0,-40,40",
+            "40,20,20",
+            "-80,-40,40"
+    })
+    void mdcP2Negativo(final Integer a, final Integer b, final Integer esp) {
+        assertEquals(esp, MathUtil.mdc(a, b));
     }
 
     @Test
-    void mdcNegativo_BP2() {
-        final Integer a = 0, b = -40, esp = 40, obt = MathUtil.mdc(a, b);
-        assertEquals(esp, obt);
-    }
-
-    @Test
-    void mdcNegativo_ALlP2() {
-        final Integer a = -80, b = -40, esp = 40, obt = MathUtil.mdc(a, b);
-        assertEquals(esp, obt);
-    }
-
-    @Test
-    void mdcZeroInBP3() {
+    void mdcP3() {
         final Integer a = 3, b = 0, esp = 3, obt = MathUtil.mdc(a, b);
         assertEquals(esp, obt);
     }
 
-    @Test
-    void mdcZeroInAP3() {
-        final Integer a = 0, b = 3, esp = 3, obt = MathUtil.mdc(a, b);
-        assertEquals(esp, obt);
-    }
 
     @Test
     void mdcM_ParP4() {
@@ -73,22 +58,33 @@ class MathUtilTest {
         assertEquals(esp, obt);
     }
 
-    @Test
-    void mdcP5() {
+    @ParameterizedTest
+    @CsvSource({
+            "7,3,5",// numero que o minimo divisor comum é 1
+            "5,3,7",
+
+    })
+    void mdcP5(final Integer a, final Integer b, final Integer c) {
         // se mdc(a,b) == 1, então mdc(a*b,c) == mdc(b,c)
-        final Integer a = 7, b = 3, c = 5, esp = mdc(a*b,c), obt = mdc(b,c);
-        assertEquals(esp,obt);
-    }
-    @Test
-    void mdcCom1P5() {
-        // se mdc(a,b) == 1, então mdc(a*b,c) == mdc(b,c)
-        final Integer a = 7, b = 3, c = 5, esp = 1, obt = mdc(b,c);
-        assertEquals(esp,obt);
+        final Integer esp = mdc(a * b, c), obt = mdc(b, c);
+        assertEquals(esp, obt);
     }
 
     @Test
-    void mdcA_BbyB_AP6() {
-        final Integer a = 4, b = 2, esp = MathUtil.mdc(b, a), obt = MathUtil.mdc(a, b);
+    void mdcCom1P5() {
+        // se mdc(a,b) == 1, então mdc(a*b,c) == mdc(b,c)
+        final Integer a = 7, b = 3, c = 5, esp = 1, obt = mdc(b, c);
+        assertEquals(esp, obt);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4,2",
+            "8,4",
+            "7,3"
+    })
+    void mdcP6(final Integer a,final Integer b) {
+        final Integer esp = mdc(b, a), obt = mdc(a, b);
         assertEquals(esp, obt);
     }
 
